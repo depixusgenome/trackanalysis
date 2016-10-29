ec2a0243fcb7110dd6f73223b7963c630a1bc612
Pol d Avezac <pol.davezac@picoseq.com>
2016-10-29 23:05:39 +0200

    Dealing with redos

--- src/view/undo.py
+++ src/view/undo.py
@@ -1,68 +1,93 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u"Deals with undos"
-from typing         import NamedTuple, Callable
+from functools      import wraps
 from collections    import deque
 from time           import sleep
 from .              import View
 
-UndoAction = NamedTuple('UndoAction', [('name', str), ('undo', Callable[[], None])])
-
 class UndoView(View):
     u"View listing all undos"
-    _uqueue    = None # type: deque
-    _rqueue    = None # type: deque
-    _isundoing = None # type: bool
+    __uqueue    = None # type: deque
+    __rqueue    = None # type: deque
+    __isundoing = None # type: bool
 
     def setCtrl(self, ctrl):
         u"sets up the observations"
-        self._isundoing = False
-        self._uqueue    = deque(maxlen = 1000)
-        self._rqueue    = deque(maxlen = 1000)
+        self.__isundoing = False
+        self.__uqueue    = deque(maxlen = 1000)
+        self.__rqueue    = deque(maxlen = 1000)
         super().setCtrl(ctrl)
 
-        ctrl.observe(self._onOpenTrack, self._onCloseTrack,
-                     self._onAddTask,   self._onUpdateTask, self._onDeleteTask)
+        self.__observe(ctrl)
+        self.__connect(getattr(self, 'connect', lambda *_: None))
 
-    def _append(self, fcn):
-        if self._isundoing:
-            self._rqueue.append(fcn)
-        else:
-            self._uqueue.append(fcn)
-            self._rqueue.clear()
+    def __observe(self, ctrl):
+        u"sets-up observer methods: depixus event loop"
+        # pylint: disable=unused-variable
+        def _onOpenTrack(controller = None, model = None, **_):
+            task = model[0]
+            return lambda: controller.closeTrack(task)
 
-    def _onOpenTrack(self, controller = None, model = None, **_):
-        task = model[0]
-        self._append(lambda: controller.closeTrack(task))
+        def _onCloseTrack(controller = None, model = None, **_):
+            return lambda: controller.openTrack(model[0], model)
 
-    def _onCloseTrack(self, controller = None, model = None, **_):
-        self._append(lambda: controller.openTrack(model[0], model))
+        def _onAddTask(controller = None, parent = None, task = None, **_):
+            return lambda: controller.removeTask(parent, task)
 
-    def _onAddTask(self, controller = None, parent = None, task = None, **_):
-        self._append(lambda: controller.removeTask(parent, task))
+        def _onUpdateTask(controller = None, parent = None, task = None,  old = None, **_):
+            return lambda: controller.updateTask(parent, task, **old)
 
-    def _onUpdateTask(self, controller = None, parent = None, task = None,  old = None, **_):
-        self._append(lambda: controller.updateTask(parent, task, **old))
+        def _onDeleteTask(controller = None, parent = None, task = None,  old = None, **_):
+            ind = old.index(task)
+            return lambda: controller.addTask(parent, task, ind)
 
-    def _onDeleteTask(self, controller = None, parent = None, task = None,  old = None, **_):
-        ind = old.index(task)
-        self._append(lambda: controller.addTask(parent, task, ind))
+        def _do(fcn):
+            @wraps(fcn)
+            def _wrap(**kwargs):
+                if self.__isundoing:
+                    self.__rqueue.append(fcn(**kwargs))
+                else:
+                    self.__uqueue.append(fcn(**kwargs))
+                    self.__rqueue.clear()
+            return _wrap
+
+        ctrl.observe([_do(fcn) for name, fcn in locals().items() if name[:3] == '_on'])
+
+    def __connect(self, connect):
+        u"sets-up connect methods: flexx event loop"
+        def _onKeyPress(*evt):
+            if len(evt) > 1:
+                return
+
+            cur = '-'.join(evt[0].modifiers)+'-'+evt[0].key
+            if   cur == self._ctrl.getConfig("keypress.undo"):
+                self.undo()
+            elif cur == self._ctrl.getConfig("keypress.redo"):
+                self.redo()
+
+        connect("key_press", _onKeyPress)
 
     def undo(self):
         u"undoes one action"
-        while self._isundoing:
+        if len(self.__uqueue) == 0:
+            return
+
+        while self.__isundoing:
             sleep(.01)
 
         try:
-            self._isundoing = True
-            nbu, nbr = len(self._uqueue), len(self._rqueue)
+            self.__isundoing = True
+            nbu, nbr = len(self.__uqueue), len(self.__rqueue)
 
-            self._uqueue.pop()[-1]()
+            self.__uqueue.pop()()
 
-            assert (nbu-1, nbr+1) == (len(self._uqueue), len(self._rqueue))
+            assert (nbu-1, nbr+1) == (len(self.__uqueue), len(self.__rqueue))
         finally:
-            self._isundoing = False
+            self.__isundoing = False
 
     def redo(self):
         u"redoes one action"
-        self._rqueue.pop()[-1]()
+        if len(self.__rqueue) == 0:
+            return
+        self.__rqueue.pop()()
