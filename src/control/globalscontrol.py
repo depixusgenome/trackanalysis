1e34473cc51e89aeefa1f1b8e1ca9e5b44e77bdb
Pol d Avezac <pol.davezac@picoseq.com>
2016-11-07 10:03:27 +0100

    refactoring

--- src/control/globalscontrol.py
+++ src/control/globalscontrol.py
@@ -11,67 +11,94 @@ from .event         import Controller, NoEmission
 
 ReturnPair = namedtuple('ReturnPair', ['old', 'value'])
 delete     = type('delete', tuple(), dict()) # pylint: disable=invalid-name
+
+class DefaultsMap(ChainMap):
+    u"Dictionnary with defaults values. It can be reset to these."
+    def __init__(self, cnt = 2):
+        self._versions = [dict() for i in range(cnt)]
+        super().__init__(*self._versions)
+
+    def updateVersion(self, *args, version = None, **kwargs):
+        u"adds defaults to the config"
+        if version is None:
+            version = -1
+        self._versions[version].update(*args, **kwargs)
+
+    def reset(self, version = None):
+        u"resets to default values"
+        if version is None:
+            version = -1
+        for i in range(version):
+            self._versions[i].clear()
+
+class DefaultsMapController(DefaultsMap): # pylint: disable=too-many-ancestors
+    u"Controller aspects of the defaultmap"
+    def update(self, *args, **kwargs):
+        u"updates keys or raises NoEmission"
+        kwargs.update(args)
+        ret = dict(empty = delete)
+        for key, val in kwargs.items():
+            old = super().get(key, delete)
+            if val is delete:
+                self.pop(key, None)
+            else:
+                self[key] = val
+
+            if old != val:
+                ret[key] = ReturnPair(old, val)
+        if len(ret):
+            return ret
+        else:
+            raise NoEmission("Config is unchanged")
+
+    def get(self, keys, default = delete):
+        u"returns values associated to the keys"
+        if default is not delete:
+            if len(keys) == 1:
+                return super().get(keys[0], default)
+            return iter(super().get(key, val) for key, val in zip(keys, default))
+        else:
+            if len(keys) == 1:
+                return self[keys[0]]
+            return iter(self[key] for key in keys)
+
 class GlobalsController(Controller):
     u"Data controller class"
     def __init__(self):
         super().__init__()
-        self.__defaults = {
-            "keypress.undo": "Ctrl-z",
-            "keypress.redo": "Ctrl-y",
-            "keypress.open": "Ctrl-o",
-            "keypress.save": "Ctrl-s",
-            "keypress.quit": "Ctrl-q",
-            "plot.bead.tools"      : 'xpan,box_zoom,undo,redo,reset,save',
-            "plot.bead.z.color"    : 'blue',
-            "plot.bead.z.glyph"    : 'circle',
-            "plot.bead.z.size"     : 1,
-            "plot.bead.zmag.color" : 'red',
-            "plot.bead.zmag.glyph" : 'line',
-            "plot.bead.zmag.size"  : 1,
-            }
-        self.__config   = ChainMap(dict(), self.__defaults)
-        self.__project  = {}
+        self.__config   = DefaultsMapController()
+        self.__config.updateVersion({"keypress.undo": "Ctrl-z",
+                                     "keypress.redo": "Ctrl-y",
+                                     "keypress.open": "Ctrl-o",
+                                     "keypress.save": "Ctrl-s",
+                                     "keypress.quit": "Ctrl-q"})
+        self.__project  = DefaultsMapController()
 
-    @staticmethod
-    def __update(items, args, kwargs):
-        kwargs.update(args)
-        ret = dict(empty = delete)
-        for key, val in kwargs.items():
-            old = items.get(key, delete)
-            if val is delete:
-                items.pop(key, None)
-            else:
-                items[key] = val
+    def updateConfigDefault(self, *args, version = None, **kwargs):
+        u"adds defaults to the config"
+        self.__config.updateVersion(*args, version = version, **kwargs)
 
-            if old != val:
-                ret[key] = ReturnPair(old, val)
+    def updateProjectDefault(self, *args, version = None, **kwargs):
+        u"adds defaults to the project"
+        self.__project.updateVersion(*args, version = version, **kwargs)
 
-        if len(ret):
-            return ret
-        else:
-            raise NoEmission()
+    def resetConfig(self, *args, version = None, **kwargs):
+        u"adds defaults to the config"
+        self.__config.reset(*args, version = version, **kwargs)
 
-    @staticmethod
-    def __get(items, keys, default = delete):
-        u"returns values associated to the keys"
-        if default is not delete:
-            if len(keys) == 1:
-                return items.get(keys[0], default)
-            return iter(items.get(key, val) for key, val in zip(keys, default))
-        else:
-            if len(keys) == 1:
-                return items[keys[0]]
-            return iter(items[key] for key in keys)
+    def resetProject(self, *args, version = None, **kwargs):
+        u"adds defaults to the project"
+        self.__project.reset(*args, version = version, **kwargs)
 
     @Controller.emit
     def updateGlobal(self, *args, **kwargs) -> dict:
         u"updates view information"
-        return self.__update(self.__project, args, kwargs)
+        return self.__project.update(*args, **kwargs)
 
     @Controller.emit
     def updateConfig(self, *args, **kwargs) -> dict:
         u"updates view information"
-        return self.__update(self.__config, args, kwargs)
+        return self.__config.update(*args, **kwargs)
 
     def deleteGlobal(self, *key):
         u"removes view information"
@@ -83,8 +110,8 @@ class GlobalsController(Controller):
 
     def getGlobal(self, *keys, default = delete):
         u"returns values associated to the keys"
-        return self.__get(self.__project, keys, default)
+        return self.__project.get(keys, default)
 
     def getConfig(self, *keys, default = delete):
         u"returns values associated to the keys"
-        return self.__get(self.__config, keys, default)
+        return self.__config.get(keys, default)
--- src/view/trackplot.py
+++ src/view/trackplot.py
@@ -6,51 +6,72 @@ from bokeh.plotting import figure
 from bokeh.models   import LinearAxis, Range1d
 from .              import FlexxView, ui
 
-def plotbead(ctrl):
-    u"sets-up the figure"
-    task = ctrl.getGlobal("current.track", default = None)
-    if task is None:
-        return figure(tools = [])
 
-    key   = 'plot.bead.'
-    items = next(iter(ctrl.run(task, task)))
-    bead  = ctrl.getGlobal("current.bead", default = None)
-    if bead is None:
-        bead = next(iter(items.keys()))
+class BeadPlotter:
+    u"Plots a default bead"
+    _KEY = 'plot.bead.'
+    def __init__(self, ctrl):
+        ctrl.updateConfigDefault({
+            self._KEY+"tools"      : 'xpan,box_zoom,undo,redo,reset,save',
+            self._KEY+"z.color"    : 'blue',
+            self._KEY+"z.glyph"    : 'circle',
+            self._KEY+"z.size"     : 1,
+            self._KEY+"zmag.color" : 'red',
+            self._KEY+"zmag.glyph" : 'line',
+            self._KEY+"zmag.size"  : 1,
+            })
+        self._ctrl = ctrl
 
-    def _add(beadname, beadvalues, **kwargs):
-        glyph = ctrl.getConfig(key+beadname+".glyph")
-        yvals = items[beadvalues]
-        args  = dict(x     = items['t'],
-                     y     = yvals,
-                     color = ctrl.getConfig(key+beadname+".color"),
-                     size  = ctrl.getConfig(key+beadname+".size"),
-                     **kwargs
+    def get(self, *key, **kwa):
+        u"returns config values"
+        return self._ctrl.getConfig(self._KEY+'.'.join(key), **kwa)
+
+    def __call__(self):
+        u"sets-up the figure"
+        task = self._ctrl.getGlobal("current.track", default = None)
+        if task is None:
+            return figure(tools = [])
+
+        items = next(iter(self._ctrl.run(task, task)))
+        bead  = self._ctrl.getGlobal("current.bead", default = None)
+        if bead is None:
+            bead = next(iter(items.keys()))
+
+        def _add(beadname, beadvalues, **kwargs):
+            glyph = self.get(beadname, "glyph")
+            yvals = items[beadvalues]
+            args  = dict(x     = items['t'],
+                         y     = yvals,
+                         color = self.get(beadname,"color"),
+                         size  = self.get(beadname,"size"),
+                         **kwargs
+                        )
+            if glyph == 'line':
+                args['line_width'] = args.pop('size')
+
+            if 'y_range_name' in kwargs:
+                vmin = yvals.min()
+                vmin = vmin * (.95  if vmin > 0. else 1./.95)
+                vmax = yvals.max()
+                vmax = vmax * (1./.95 if vmax > 0. else .95)
+                inte = vmax-vmin
+                fig.extra_y_ranges = {beadname: Range1d(start        = vmin,
+                                                        end          = vmax,
+                                                        min_interval = inte,
+                                                        max_interval = inte
+                                                       )}
+
+                fig.add_layout(LinearAxis(y_range_name=beadname), 'right')
+
+            getattr(fig, glyph)(**args)
+
+        fig = figure(tools = self.get("tools"),
+                     x_axis_label = 'Time',
+                     y_axis_label = 'z',
                     )
-        if glyph == 'line':
-            args['line_width'] = args.pop('size')
-
-        if 'y_range_name' in kwargs:
-            vmin = yvals.min()
-            vmin = vmin * (.95  if vmin > 0. else 1./.95)
-            vmax = yvals.max()
-            vmax = vmax * (1./.95 if vmax > 0. else .95)
-            inte = vmax-vmin
-            fig.extra_y_ranges = {beadname: Range1d(start = vmin, end = vmax,
-                                                    min_interval = inte,
-                                                    max_interval = inte)}
-
-            fig.add_layout(LinearAxis(y_range_name=beadname), 'right')
-
-        getattr(fig, glyph)(**args)
-
-    fig = figure(tools = ctrl.getConfig(key+"tools"),
-                 x_axis_label = 'Time',
-                 y_axis_label = 'z',
-                )
-    _add("z", bead)
-    _add("zmag", "zmag", y_range_name = 'zmag')
-    return fig
+        _add("z", bead)
+        _add("zmag", "zmag", y_range_name = 'zmag')
+        return fig
 
 class TrackPlot(FlexxView):
     u"Track plot view"
@@ -59,10 +80,11 @@ class TrackPlot(FlexxView):
 
     def observe(self, ctrl):
         super().observe(ctrl)
+        plotter = BeadPlotter(ctrl)
 
         plot = self._plot
         def _onUpdateGlobal(**items):
             if 'current.track' in items or 'current.bead' in items:
-                plot.plot = plotbead(ctrl)
+                plot.plot = plotter()
 
         ctrl.observe(_onUpdateGlobal)
