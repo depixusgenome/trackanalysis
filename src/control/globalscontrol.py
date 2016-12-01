9ffabce502a02d55474a92e740903d349bf80649
Pol d Avezac <pol.davezac@picoseq.com>
2016-12-01 21:38:30 +0100

    adding sugar

--- src/control/globalscontrol.py
+++ src/control/globalscontrol.py
@@ -30,6 +30,63 @@ from .event         import Controller
 ReturnPair = namedtuple('ReturnPair', ['old', 'value'])
 delete     = type('delete', tuple(), dict())    # pylint: disable=invalid-name
 
+def _tokwargs(args, kwargs):
+    if len(args) == 1 and isinstance(args[0], dict):
+        kwargs.update(args)
+    else:
+        kwargs.update(*args)
+    return kwargs
+
+class _MapGetter:
+    value    = property(lambda self: self._ctrl.get(self._key),
+                        lambda self, val: self._ctrl.update(self._base, val),
+                        lambda self: self._ctrl.pop(self._base))
+    values   = property(lambda self: self._ctrl.values(self._key),
+                        lambda self, val: self.set(**val),
+                        lambda self: self._ctrl.update(*self.values))
+    defaults = property(None, lambda self, val: self.setdefaults(**val))
+
+    def __init__(self, ctrl, key):
+        self._ctrl  = ctrl
+        self._key   = key
+
+    def __getattr__(self, name):
+        if self._key == '':
+            return _MapGetter(self._ctrl, name)
+        else:
+            self._key += '.'+name
+            return self
+    __getitem__ = __getattr__
+
+    def __setattr__(self, name, value):
+        self._ctrl.update(self._base+'.'+name, value)
+    __setitem__ = __setattr__
+
+    def __delattr__(self, name):
+        self._ctrl.pop(self._base+'.'+name)
+    __delitem__ = __delattr__
+
+
+    def __kwargs(self, args, kwargs):
+        kwargs = {self._key+'.'+name: value
+                  for name, value in _tokwargs(args, kwargs).items()}
+
+    def setdefaults(self, *args, version = 1, **kwargs):
+        u"Calls update using the current base key"
+        self._ctrl.setdefaults(self.__kwargs(args, kwargs), version = version)
+
+    def set(self, *args, **kwargs):
+        u"Calls update using the current base key"
+        self._ctrl.update(self.__kwargs(args, kwargs))
+
+    def pop(self, *keys):
+        u"Calls get using the current base key"
+        self._ctrl.pop(*(self._key+'.'+i for i in keys))
+
+    def get(self, *keys, default = delete):
+        u"Calls get using the current base key"
+        self._ctrl.get(*(self._key+'.'+i for i in keys), default = default)
+
 class DefaultsMap(Controller):
     u"Dictionnary with defaults values. It can be reset to these."
     _CNT = 2
@@ -48,12 +105,7 @@ class DefaultsMap(Controller):
 
     def setdefaults(self, *args, version = 1, **kwargs):
         u"adds defaults to the config"
-        if len(args) == 1 and isinstance(args[0], dict):
-            kwargs.update(*args)
-        else:
-            kwargs.update(args)
-
-        self.__items.maps[version].update(**kwargs)
+        self.__items.maps[version].update(**_tokwargs(args, kwargs))
 
     def reset(self, version = None):
         u"resets to default values"
@@ -64,13 +116,8 @@ class DefaultsMap(Controller):
 
     def update(self, *args, **kwargs) -> dict:
         u"updates keys or raises NoEmission"
-        if len(args) == 1 and isinstance(args[0], dict):
-            kwargs.update(*args)
-        else:
-            kwargs.update(args)
-
         ret = dict(empty = delete) # type: Dict[str,Union[type,ReturnPair]]
-        for key, val in kwargs.items():
+        for key, val in _tokwargs(args, kwargs).items():
             old = self.__items.get(key, delete)
             if val is delete:
                 self.__items.pop(key, None)
@@ -86,6 +133,18 @@ class DefaultsMap(Controller):
     def pop(self, *args):
         u"removes view information"
         return self.update(dict.fromkeys(args, delete))
+
+    def keys(self, base = ''):
+        u"returns all keys starting with base"
+        return iter(key for key in self.__items if key.startswith(base))
+
+    def values(self, base = ''):
+        u"returns all values with keys starting with base"
+        return iter(val for key, val in self.__items.items() if key.startswith(base))
+
+    def items(self, base = ''):
+        u"returns all items with keys starting with base"
+        return iter(key for key in self.__items.items() if key[0].startswith(base))
 
     def get(self, *keys, default = delete):
         u"returns values associated to the keys"
@@ -104,32 +163,44 @@ class DefaultsMap(Controller):
             return iter(self.__items[key] for key in keys)
 
 class GlobalsController(Controller):
-    u"Data controller class"
+    u"""
+    Controller class for global values.
+    These can be accessed using a main key and secondary keys:
+
+    >> # Get the secondary key 'keypress.pan.x' in 'plot'
+    >> ctrl.getGlobal('plot').keypress.pan.x.low.value
+
+    >> # Get the secondary keys 'keypress.pan.x.low' and 'high'
+    >> ctrl.getGlobal('plot').keypress.pan.x.get('low', 'high')
+
+    >> # Get secondary keys starting with 'keypress.pan.x'
+    >> ctrl.getGlobal('plot').keypress.pan.x.values
+    """
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.__maps = dict()
-        self.addGlobalMap('css',
-                          **{'button.width' : 90})
-        self.addGlobalMap('config',
-                          **{'keypress.undo' : "Control-z",
-                             'keypress.redo' : "Control-y",
-                             'keypress.open' : "Control-o",
-                             'keypress.save' : "Control-s",
-                             'keypress.quit' : "Control-q"})
-        self.addGlobalMap('config.plot',
-                          tools = 'xpan,wheel_zoom,box_zoom,reset,save',
-                          **{'panning.speed'       : .2,
-                             'zooming.speed'       : .2,
-                             'boundary.overshoot'  : .005,
-                             'keypress.x.pan.low'  : 'ArrowLeft',
-                             'keypress.x.pan.high' : 'ArrowRight',
-                             'keypress.x.zoom.out' : 'Shift-ArrowLeft',
-                             'keypress.x.zoom.in'  : 'Shift-ArrowRight',
-                             'keypress.y.pan.low'  : 'ArrowDown',
-                             'keypress.y.pan.high' : 'ArrowUp',
-                             'keypress.y.zoom.out' : 'Shift-ArrowDown',
-                             'keypress.y.zoom.in'  : 'Shift-ArrowUp',
-                             'keypress.reset'      : ' '})
+        self.addGlobalMap('css').button.width = 90
+        self.addGlobalMap('config').keypress.defaults = {'undo' : "Control-z",
+                                                         'redo' : "Control-y",
+                                                         'open' : "Control-o",
+                                                         'save' : "Control-s",
+                                                         'quit' : "Control-q"}
+        def _gesture(meta):
+            return {'speed'   : .2,
+                    'activate': meta,
+                    'x.low'   : meta+'ArrowLeft',
+                    'x.high'  : meta+'ArrowRight',
+                    'y.low'   : meta+'ArrowDown',
+                    'y.high'  : meta+'ArrowUp'}
+
+        item = self.addGlobalMap('config.plot')
+        item.tools              = 'xpan,box_zoom,reset,save'
+        item.boundary.overshoot = .005
+        item.keypress.reset     = ' '
+        item.keypress.pan       =  _gesture('')
+        item.keypress.zoom      =  _gesture('Shift')
+
         self.addGlobalMap('current')
         self.addGlobalMap('current.plot')
 
@@ -142,6 +213,7 @@ class GlobalsController(Controller):
             self.__maps[key] = DefaultsMap(key, handlers = self._handlers)
 
         self.__maps[key].setdefaults(*args, **kwargs)
+        return self.__maps[key]
 
     def removeGlobalMap(self, key):
         u"removes a map"
@@ -162,5 +234,5 @@ class GlobalsController(Controller):
     def getGlobal(self, key, *args, default = delete):
         u"returns values associated to the keys"
         if len(args) == 0:
-            return self.__maps[key]
+            return _MapGetter(self.__maps[key], '')
         return self.__maps[key].get(*args, default = default)
