4e6f9a5447be6961d6475716a2fb1077d3b11ead
Pol d Avezac <pol.davezac@depixus.com>
2016-12-09 16:03:25 +0100

    setting attributes to private

--- src/view/toolbar.py
+++ src/view/toolbar.py
@@ -11,47 +11,47 @@ class  ToolBar(BokehView):
     def __init__(self, **kwa):
         u"Sets up the controller"
         super().__init__(**kwa)
-        self._tools    = [self.button(self._onOpen,  u'open'),
-                          self.button(self._onSave,  u'save', disabled = True)]
+        self.__tools = [self.button(self._onOpen,  u'open'),
+                        self.button(self._onSave,  u'save', disabled = True)]
         if self._ctrl.ISAPP:
-            self._tools.append(self.button(self._ctrl.close, u'quit'))
+            self.__tools.append(self.button(self._ctrl.close, u'quit'))
 
-        self._diagopen = FileDialog(filetypes = u'trk|ana|*',
-                                    config    = self._ctrl,
-                                    title     = u'Open a track or analysis file')
-        self._diagsave = FileDialog(filetypes = u'ana|*',
-                                    config    = self._ctrl,
-                                    title     = u'Save an analysis file')
+        self.__diagopen = FileDialog(filetypes = u'trk|ana|*',
+                                     config    = self._ctrl,
+                                     title     = u'Open a track or analysis file')
+        self.__diagsave = FileDialog(filetypes = u'ana|*',
+                                     config    = self._ctrl,
+                                     title     = u'Save an analysis file')
 
         self._ctrl.observe("globals.current", self._onUpdateCurrent)
 
     def getroots(self):
         u"adds items to doc"
-        return Row(children = self._tools, sizing_mode = 'fixed'),
+        return Row(children = self.__tools, sizing_mode = 'fixed'),
 
     def close(self):
         u"Sets up the controller"
         super().close()
-        del self._tools
-        del self._diagopen
-        del self._diagsave
+        del self.__tools
+        del self.__diagopen
+        del self.__diagsave
 
     def _onUpdateCurrent(self, **items):
         if 'track' not in items:
             return
-        self._tools[1].disabled = items['track'].value is items['empty']
+        self.__tools[1].disabled = items['track'].value is items['empty']
 
     @BokehView.action
     def _onOpen(self, *_):
-        path  = self._diagopen.open()
+        path  = self.__diagopen.open()
         if path is not None:
             self._ctrl.openTrack(path)
 
     @BokehView.action
     def _onSave(self,  *_):
-        if self._tools[1].disabled:
+        if self.__tools[1].disabled:
             return
 
-        path = self._diagsave.save()
+        path = self.__diagsave.save()
         if path is not None:
             self._ctrl.saveTrack(path)
