a1c43aacbb4ba0e64ca416b3ba2b3d9105325b41
Pol d Avezac <pol.davezac@depixus.com>
2016-12-19 15:52:52 +0100

    missing import ._core

--- src/signalfilter/__init__.py
+++ src/signalfilter/__init__.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u"Signal Analysis: filters, stats and interval detection"
+from ._core import ForwardBackwardFilter, NonLinearFilter # pylint: disable=no-name-in-module
