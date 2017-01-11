2aa77c6fb104670bc91c5ba422c541098e07d937
Pol d Avezac <pol.davezac@depixus.com>
2017-01-11 11:08:14 +0100

    added doctest

--- src/sequences/__init__.py
+++ src/sequences/__init__.py
@@ -4,31 +4,48 @@ u"All sequences-related stuff"
 from    typing import (Sequence, Union,  # pylint: disable=unused-import
                        Iterator, Tuple, TextIO)
 import  pathlib
+import  re
 import  numpy       as np
 import  Bio.SeqIO   as seqio
 from    Bio.Seq     import Seq
 from    utils       import fromstream
 
 @fromstream
-def read(stream:TextIO) -> 'Iterator[Tuple[str,np.ndarray]]':
+def read(stream:TextIO) -> 'Iterator[Tuple[str,str]]':
     u"reads a path and yields pairs (name, sequence)"
-    yield from ((name, np.array(val, dtype = '<U4'))
-                for name, val in seqio.FastaIO.SimpleFastaParser(stream))
+    yield from seqio.FastaIO.SimpleFastaParser(stream)
 
 def peaks(seq:str, oligs:'Sequence[str]') -> np.ndarray:
-    u"returns the peaks associated to a sequence"
-    sequence = np.array(seq, dtype = '<U4', copy = False)
+    u"""
+    Returns the peak positions and orientation associated to a sequence.
+    
+    A peak position is the end position of a match. With indexes starting at 0,
+    that's the indexe of the first base *after* the match.
+    
+    The orientation is *True* if the oligo was matched and false otherwise. Palindromic
+    cases are *True*.
 
+    Matches are **case sensitive**.
+
+    Example:
+
+    >>> import numpy as np
+    >>> seq   = "atcgATATATatcgCCCaaGGG"
+    >>> peaks = peaks(seq, ('ATAT', 'CCC'))
+    >>> assert len(peaks) == 4
+    >>> assert all(a == b for a, b in zip(peaks['position'],    [8, 10, 17, 22]))
+    >>> assert all(a == b for a, b in zip(peaks['orientation'], [True]*3+[False]))
+    """
     def _get(elems, state):
-        for olig in elems:
-            nol  = len(olig)
-            nseq = (len(sequence)//nol)*nol
-            for i in range(nol):
-                vals = sequence[i:i+nseq].reshape((nseq//nol,nol)) == olig
-                yield from ((j+1, state) for j in np.nonzero(vals)[0])
+        reg = re.compile('|'.join(elems))
+        val = reg.search(seq, 0)
+        while val is not None:
+            yield (val.end(), state)
+            val = reg.search(seq, val.start()+1)
 
     vals = dict(_get((str(Seq(i).reverse_complement()) for i in oligs), False))
     vals.update(_get((i for i in oligs), True))
 
-    return np.array(vals.items(),
-                    dtype = [('position', np.int32), ('orientation', np.bool8)])
+    return np.fromiter(sorted(vals.items()),
+                       dtype = [('position', np.int32), ('orientation', np.bool8)],
+                       count = len(vals))
