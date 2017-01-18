540f5d866f0d11479aa4ba9da7a888ae044e6ac1
Pol d Avezac <pol.davezac@depixus.com>
2017-01-18 10:06:39 +0100

    moving code around

---
+++ tests/cordrifttest.py
@@ -0,0 +1,60 @@
+import cordrift.collapse as collapse
+
+def test_collapse_intervals():
+    u"Tests interval collapses"
+    yvals  = numpy.zeros((100,5), dtype = numpy.float32)
+    for i in range(yvals.shape[1]):
+        yvals[:,i] = i
+    xvals   = numpy.arange(100)+100
+
+    # test horizontal lines
+    inters  = [(xvals[5:10], yvals[5:10,i]) for i in range(yvals.shape[1])]
+    prof    = collapse.intervals(iter(inters))
+    assert all(prof.count == 5)
+    assert all(prof.value == 0.)
+
+    # test slanted lines
+    yvals[5:10,:] = numpy.arange(25).reshape((5,5))  # pylint: disable=no-member
+    prof          = collapse.intervals(iter(inters[1:-1]))
+    assert all(prof.count == 3)
+    numpy.testing.assert_allclose([-10,-5,0,5,10], prof.value, rtol = 1e-4)
+
+    # test slanted lines + noise
+    yvals[5:10,:] += numpy.random.rand(25).reshape((5,5))  # pylint: disable=no-member
+    prof  = collapse.intervals(iter(inters[1:-1]))
+    assert all(prof.count == 3)
+    truth = numpy.mean(yvals[5:10,1:-1] - numpy.mean(yvals[5:10,1:-1], axis = 0), axis = 1)
+    numpy.testing.assert_allclose(truth, prof.value, rtol = 1e-4)
+
+    # test non-overlapping intervals
+    inters[0] = (xvals[15:25], yvals[15:25,1])
+    prof      = collapse.intervals(iter(inters[:-1]))
+    assert all(prof.count == ([3]*5+[0]*5+[1]*10))
+    numpy.testing.assert_allclose(truth, prof.value[:5], rtol = 1e-5)
+    assert all(prof.value[5:] == 0.)
+
+def test_collapse_derivate():
+    u"Tests derivate collapses"
+    yvals  = numpy.zeros((100,5), dtype = numpy.float32)
+    for i in range(yvals.shape[1]):
+        yvals[:,i] = i
+    xvals   = numpy.arange(100)+100
+
+    # test horizontal lines
+    inters  = [(xvals[5:10], yvals[5:10,i]) for i in range(yvals.shape[1])]
+    prof    = collapse.derivate(iter(inters))
+    assert all(prof.count == ([5]*4+[0]))
+    assert all(prof.value == 0.)
+
+    # test slanted lines
+    yvals[5:10,:] = numpy.arange(25).reshape((5,5))  # pylint: disable=no-member
+    prof          = collapse.derivate(iter(inters[1:-1]))
+    assert all(prof.count == ([3]*4+[0]))
+    numpy.testing.assert_allclose([-20,-15,-10,-5,0], prof.value, rtol = 1e-4)
+
+    # test non-overlapping intervals
+    inters[0] = (xvals[15:25], yvals[15:25,1])
+    prof      = collapse.derivate(iter(inters[:-1]))
+    assert all(prof.count == ([3]*4+[0]*6+[1]*9+[0]))
+    numpy.testing.assert_allclose([-20,-15,-10,-5,0], prof.value[:5], rtol = 1e-4)
+    assert all(prof.value[5:] == 0.)
