8d9287504ec46f8c5c757d97069b19f7b821f9a6
Pol d Avezac <pol.davezac@depixus.com>
2017-01-30 22:07:39 +0100

    adding a RollingFilter class and an union type for all filters

--- src/signalfilter/__init__.py
+++ src/signalfilter/__init__.py
@@ -2,10 +2,36 @@
 # -*- coding: utf-8 -*-
 u"Signal Analysis: filters, stats and interval detection"
 # pylint: disable=no-name-in-module,import-error
+from typing         import Union
+
 import numpy as np
+
+from pandas         import Series
 from ._core         import (ForwardBackwardFilter, NonLinearFilter, samples)
 from ._core.stats   import hfsigma
 
 def nanhfsigma(arr):
     u"hfsigma which takes care of nans"
     return hfsigma(arr[~np.isnan(arr)])
+
+class RollingFilter:
+    u"interface to panda rolling window methods"
+    def __init__(self, window = 10, mode = 'mean', std = None, **_):
+        self.window = window
+        self.std    = std
+        self.mode   = mode
+
+    def __call__(self, data):
+        series = Series(data).rolling(self.window, center = True)
+        imin   = self.window//2 + (self.window % 2)
+        imax   = -self.window//2
+
+        if self.mode in ('gaussian', 'kaiser'):
+            if self.std is None:
+                raise AttributeError("'gaussian' and 'kaiser' modes need an std")
+            data[imin:imax] = getattr(series, self.mode)(std = self.std).values
+        else:
+            data[imin:imax] = getattr(series, self.mode)().values
+        return data
+
+Filter = Union[ForwardBackwardFilter, NonLinearFilter, RollingFilter]
