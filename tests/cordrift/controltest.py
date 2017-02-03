386202d1bdc5129f183c415c67677998799c53dd
Pol d Avezac <pol.davezac@depixus.com>
2017-02-03 14:11:48 +0100

    more tests, more success

--- tests/cordrift/controltest.py
+++ tests/cordrift/controltest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u"Tests that the cordrift processor runs correctly"
+import numpy as np
 from numpy.testing          import assert_allclose
 from cordrift.processor     import BeadDriftTask
 from simulator.processor    import TrackSimulatorTask
@@ -12,8 +13,16 @@ def test_process():
                             BeadDriftTask()))
     cycs = next(i[...,...] for i in pair.run()).withphases(5,5)
     for _, val in cycs:
-        val -= val[0]
+        val -= val[-1]
         assert_allclose(val, 0., atol = 1e-8)
+
+    pair = TaskPair.create((TrackSimulatorTask(brownian  = 0.),
+                            BeadDriftTask()))
+    cycs = next(i[...,...] for i in pair.run()).withphases(5,5)
+    for _, val in cycs:
+        val -= val[-1]
+        val -= np.round(val, 1)
+        assert_allclose(val, 0., atol = 1e-6)
 
 if __name__ == '__main__':
     test_process()
