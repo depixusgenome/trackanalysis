d350d38bedf14dc653bc4d1d88f27e1f677cc254
Pol d Avezac <pol.davezac@depixus.com>
2017-03-01 21:10:36 +0100

    adding BEADKEY and CYCLEKEY to the namespace

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -4,4 +4,4 @@ u"""
 Base track file data.
 """
 from .track      import Track
-from .trackitems import Beads, Cycles, TrackItems, createTrackItem
+from .trackitems import Beads, Cycles, TrackItems, createTrackItem, BEADKEY, CYCLEKEY
