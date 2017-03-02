ad63b016e48c4d65ae19944675db4c83b3182a8a
Pol d Avezac <pol.davezac@depixus.com>
2017-03-02 14:38:40 +0100

    files were merged incorrectly

--- tests/eventdetectiontest.py
+++ tests/eventdetectiontest.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 u"Tests interval detection"
 
-import numpy    # type: ignore
+import numpy  as np
 from eventdetection.detection import (SplitDetector, EventMerger, EventSelector,
-                                      ExtremumAlignment, CorrelationAlignment,
                                       tocycles)
+from eventdetection.alignment import ExtremumAlignment, CorrelationAlignment
 from signalfilter             import samples
 
 def test_detectsplits():
     u"Tests flat stretches detection"
     inst  = SplitDetector(precision = 1., confidence = 0.1, window = 1)
     det   = lambda  i: tuple(tuple(j) for j in inst(i))
-    items = numpy.zeros((30,))
+    items = np.zeros((30,))
     thr   = samples.normal.knownsigma.threshold(True, inst.confidence, inst.precision,
                                                 inst.window, inst.window)
 
@@ -29,19 +29,19 @@ def test_detectsplits():
     items[29:] += thr
     assert det(items) == ((2, 10), (10,20), (21,28))
 
-    items       = numpy.zeros((30,))
+    items       = np.zeros((30,))
     items[10:] += thr
     items[20:] += thr
     items[21:] += thr
-    items[[0, 10, 25, 29]] = numpy.nan
+    items[[0, 10, 25, 29]] = np.nan
     assert det(items) == ((0, 11), (11,20), (21,30))
 
 def _merges(oneperrange):
     u"Tests flat stretches merging"
     inst  = EventMerger(precision = 1., confidence = 0.1, isequal = True,
                         oneperrange = oneperrange)
-    det   = lambda  i, j: tuple(tuple(_) for _ in inst(i, numpy.array(j)))
-    items = numpy.zeros((30,))
+    det   = lambda  i, j: tuple(tuple(_) for _ in inst(i, np.array(j)))
+    items = np.zeros((30,))
 
     assert det([], ((0,30),)) == tuple()
     assert det([1], tuple())  == tuple()
@@ -59,7 +59,7 @@ def _merges(oneperrange):
     assert det(items, ((0, 10),(11,20),(22,30))) == ((0,10),(11,30))
 
     items[11:] = 0.
-    items[25]  = numpy.nan
+    items[25]  = np.nan
     assert det(items, ((0, 10),(11,20),(22,30))) == ((0,10),(11,30))
 
 def test_fastmerge():
@@ -74,14 +74,14 @@ def test_select():
     u"Tests flat stretches filtering"
     det   = lambda  i, j, k: tuple(tuple(_)
                                    for _ in EventSelector(edgelength = i, minlength = j)
-                                   (numpy.array(k)))
+                                   (np.array(k)))
     assert det(0, 0, ((0,0),(1,1)))                 == ((0,0), (1,1))
     assert det(0, 5, ((0,0),(1,1),(5,10)))          == ((5,10),)
     assert det(1, 5, ((0,0),(1,1),(5,10), (20,30))) == ((21,29),)
 
 def test_tocycles():
     u"Tests interval assignment to cycles"
-    starts = numpy.arange(10)*10
+    starts = np.arange(10)*10
     inters = ((0,5), (3, 5), (0,15), (2, 21), (11,15), (11, 22), (16, 33),
               (80, 89), (80, 90), (80, 91), (83, 100), (83, 101), (90,120),
               (95, 95), (88, 88))
