1e46fd26b47cd92c4a5053cec3fbe903ce264133
Pol d Avezac <pol.davezac@depixus.com>
2017-03-03 14:36:51 +0100

    merging files to reduce compilation time

--- src/simulator/processor.py
+++ src/simulator/processor.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u"Processes TrackSimulatorTask"
+from typing             import Optional     # pylint: disable=unused-import
+from utils              import initdefaults
+from model.task         import RootTask, Level
 from control.processor  import Processor
-from .task              import (TrackSimulatorTask, EventSimulatorTask,
-                                ByPeaksEventSimulatorTask)
+from .track             import TrackSimulator
+
+class _SimulatorTask(TrackSimulator):
+    u"Class indicating that a track file should be added to memory"
+    nbeads = 1      # type: int
+    seed   = None   # type: Optional[int]
+    @initdefaults
+    def __init__(self, **kwa):
+        super().__init__(**kwa)
+        RootTask.__init__(self, **kwa) # pylint: disable=non-parent-init-called
+
+class TrackSimulatorTask(_SimulatorTask, RootTask):
+    u"Class that creates fake track data each time it is called upon"
+
+class EventSimulatorTask(_SimulatorTask, RootTask):
+    u"Class that creates fake event data each time it is called upon"
+    ncycles = 20    # type: int
+    levelou = Level.event
+
+class ByPeaksEventSimulatorTask(_SimulatorTask, RootTask):
+    u"Class that creates fake peak data each time it is called upon"
+    ncycles = 20    # type: int
+    levelou = Level.peak
 
 class SimulatorMixin:
     u"Processes a simulator task"
     _FCN  = ''
-
     @staticmethod
     def _generate(sim, items):
         yield sim(*items)
@@ -21,15 +44,12 @@ class SimulatorMixin:
 
 class TrackSimulatorProcessor(SimulatorMixin, Processor):
     u"Processes TrackSimulatorTask"
-    tasktype = TrackSimulatorTask
     _FCN     = 'beads'
 
 class EventSimulatorProcessor(SimulatorMixin, Processor):
     u"Processes EventSimulatorTask"
-    tasktype = EventSimulatorTask
     _FCN     = 'bybeadevents'
 
 class ByPeaksEventSimulatorProcessor(SimulatorMixin, Processor):
     u"Processes EventSimulatorTask"
-    tasktype = ByPeaksEventSimulatorTask
     _FCN     = 'bypeakevents'
