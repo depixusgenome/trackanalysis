9c09771783c5f1525cf55f97c1a7547ca8b11012
Pol d Avezac <pol.davezac@depixus.com>
2017-03-03 20:34:26 +0100

    quicklinks for easy testing

--- tests/testingcore/__init__.py
+++ tests/testingcore/__init__.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 u""" access to files """
-import os
+from typing  import Union, Sequence
+from pathlib import Path
+
+def _trackreadertask(fpath, beadsonly = True):
+    from model.task     import TrackReaderTask
+    from data.trackio   import checkpath
+    return TrackReaderTask(path = checkpath(fpath).path, beadsonly = beadsonly)
+
+def big_selected():
+    u"returns a TrackReaderTask on 2 GR  files"
+    return _trackreadertask((Path(path("big_legacy")).parent/"*.trk",
+                             path("CTGT_selection")))
+
+def big_all():
+    u"returns a TrackReaderTask on all GR files"
+    return _trackreadertask((Path(path("big_legacy")).parent/"*.trk",
+                             path("big_grlegacy")))
 
 PATHS = dict(small_pickle   = "small_pickle.pk",
              small_legacy   = "test035_5HPs_mix_GATG_5nM_25C_8sec_with_ramp.trk",
              big_legacy     = "test035_5HPs_mix_CTGT--4xAc_5nM_25C_10sec.trk",
              big_grlegacy   = "test035_5HPs_mix_CTGT--4xAc_5nM_25C_10sec",
-             ramp_legacy    = "ramp_5HPs_mix.trk")
+             ramp_legacy    = "ramp_5HPs_mix.trk",
+             big_selected   = big_selected,
+             big_all        = big_all)
 
-def path(name:str) -> str:
+def path(name:str) -> Union[str, Sequence[str]]:
     u"returns the path to the data"
-    val = "../tests/"+__package__+"/"+PATHS.get(name.lower().strip(), name)
-    if not os.path.exists(val):
-        raise KeyError("Check your file name!!! "+val)
-    return os.path.abspath(val)
+    default = PATHS.get(name.lower().strip(), name)
+    if callable(default):
+        return default()
+
+    def _test(i):
+        val = Path("../tests/"+__package__+"/"+i)
+        if not val.exists():
+            raise KeyError("Check your file name!!! {}".format(val))
+        return val.absolute()
+
+    if isinstance(default, tuple):
+        return tuple(_test(i) for i in default)
+    else:
+        return _test(default)
