99081c459a6952013eee31352388ff089f8e34ec
Pol d'Avezac <pol.davezac@depixus.com>
2017-04-12 10:56:35 +0200

    removing pylint error

--- src/signalfilter/__init__.py
+++ src/signalfilter/__init__.py
@@ -6,4 +6,4 @@ from ._core             import samples # pylint: disable=no-name-in-module,impor
 from .noisereduction    import (RollingFilter, NonLinearFilter,
                                 ForwardBackwardFilter, Filter)
 
-rawprecision = PrecisionAlg.rawprecision
+rawprecision = PrecisionAlg.rawprecision # pylint: disable=invalid-name
