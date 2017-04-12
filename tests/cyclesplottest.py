18b83d258d86a7a900ddf2b815136e3013a43d5c
Pol d Avezac <pol.davezac@depixus.com>
2017-04-12 10:42:44 +0200

    splitting tests

---
+++ tests/cyclesplottest.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+""" Tests cycles views """
+from pytest                     import approx       # pylint: disable=no-name-in-module
+
+from testingcore.bokehtesting   import bokehaction  # pylint: disable=unused-import
+from view.plots                 import DpxKeyedRow
+
+def test_cyclesplot(bokehaction):
+    "test cyclesplot basic stuff"
+    import anastore
+    vals = [0.]*2
+    def _printrng(evts):
+        if 'y' in evts:
+            vals[:2] = evts['y'].value
+
+    with bokehaction.launch('cyclesplot.CyclesPlotView', 'app.BeadToolBar') as server:
+        server.ctrl.getGlobal('config').tasks.default = []
+        server.ctrl.observe("globals.project.plot.cycles", _printrng)
+        server.load('big_legacy')
+
+        krow = next(iter(server.doc.select(dict(type = DpxKeyedRow))))
+        def _press(val, *truth):
+            server.press(val, krow)
+            assert vals == approx(truth, rel = 1e-2)
+
+        fig  = server.widget['Cycles:Hist']()
+        for _ in range(5):
+            if fig.extra_x_ranges['cycles'].end is None:
+                server.wait()
+        assert fig.x_range.end                  > 2000.
+        assert fig.extra_x_ranges['cycles'].end > 30.
+
+        _press('Shift- ',           0., 0.)
+
+
+        _press('Shift-ArrowUp',    0.258410, 0.464449)
+        assert fig.x_range.end                  == approx(103, abs=.1)
+        assert fig.extra_x_ranges['cycles'].end == approx(4,   abs=.1)
+
+        _press('Alt-ArrowUp',       0.299618, 0.505657)
+        _press('Alt-ArrowDown',     0.258410, 0.464449)
+        _press('Shift-ArrowDown',  -0.153668, 0.876528)
+        curr = server.ctrl.getGlobal("project")
+        assert curr.bead in (None, 0)
+        server.press('PageUp')
+        assert curr.bead == 1
+
+        server.change('Cycles:Oligos', 'value', ' TGGC  , aatt')
+        assert server.widget['Cycles:Oligos'].value == 'aatt, tggc'
+        cnf = anastore.load(server.ctrl.configpath(next(anastore.iterversions('config'))))
+        assert cnf['config']['oligos'] == ['aatt', 'tggc']
+        assert cnf['config.plot']['oligos.history'] == [['aatt', 'tggc']]
+
+        server.change('Cycles:Oligos', 'value', '')
+        assert server.widget['Cycles:Oligos'].value == ''
+        cnf = anastore.load(server.ctrl.configpath(next(anastore.iterversions('config'))))
+        assert 'oligos' not in cnf.get('config', {})
+        assert cnf['config.plot']['oligos.history'] == [['aatt', 'tggc']]
+
+        server.load('hairpins.fasta', andpress = False)
+        server.change('Cycles:Sequence', 'value', '←')
+        assert server.widget['Cycles:Hist'].ygrid[0].ticker.usedefault is True
+        assert (server.widget['Cycles:Peaks'].source.data['bases']
+                == approx([0, 1000], abs = 1.))
+
+        server.change('Cycles:Oligos',   'value', 'TgGC ')
+        assert server.widget['Cycles:Hist'].ygrid[0].ticker.usedefault is False
+        assert (server.widget['Cycles:Peaks'].source.data['bases']
+                == approx([166, 1113], abs = 1.))
+
+        assert server.widget['Cycles:Stretch'].value == approx(1./8.8e-4, abs = 1e-1)
+        assert server.widget['Cycles:Bias'].value == approx(-.092152, abs = 1e-5)
+        server.change('Cycles:Bias',     'value', -.05)
+        assert server.widget['Cycles:Bias'].value == approx(-.05, abs = 1e-5)
+        server.change('Cycles:Stretch',  'value', 1050.)
+        assert server.widget['Cycles:Stretch'].value == approx(1050., abs = 1e-5)
+        server.press('Control-z')
+
+def test_cyclesplot2(bokehaction):
+    "test cyclesplot data actions"
+
+    with bokehaction.launch('cyclesplot.CyclesPlotView', 'app.BeadToolBar') as server:
+        server.ctrl.getGlobal('config').tasks.default = []
+        server.load('big_legacy')
+
+        fig  = server.widget['Cycles:Hist']()
+        assert fig.extra_x_ranges['cycles'].end < 50
+        server.change('Cycles:Alignment', 'active', 1)
+
+        for _ in range(5):
+            val = fig.extra_x_ranges['cycles'].end
+            if val is not None and val > 80:
+                break
+            server.wait()
+        assert server.widget['Cycles:Alignment'].active == 1
+        assert fig.extra_x_ranges['cycles'].end > 80
+
+        server.press('Control-z')
+        for _ in range(5):
+            val = fig.extra_x_ranges['cycles'].end
+            if val is not None and val < 80:
+                break
+            server.wait()
+        assert server.widget['Cycles:Alignment'].active == 0
+        assert fig.extra_x_ranges['cycles'].end < 50
+
+        rng  = server.widget['Cycles:Raw']().x_range
+        vals = rng.start, rng.end
+
+        server.change('Cycles:EventDetection', 'active', [0], browser = False)
+        for _ in range(5):
+            val = rng.end
+            if val is not None and val < 400:
+                break
+            server.wait()
+        assert rng.start > vals[0]
+        assert rng.end   < vals[1]
+        assert rng.end   < 350
+
+if __name__ == '__main__':
+    test_cyclesplot(bokehaction(None))
---
+++ tests/hybridstatviewtest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+""" Tests views """
+from pytest                     import approx       # pylint: disable=no-name-in-module
+import numpy as np
+
+from testingcore.bokehtesting   import bokehaction  # pylint: disable=unused-import
+from view.plots                 import DpxKeyedRow
+
+def test_peaksplot(bokehaction):
+    "test peaksplot"
+    vals = [0.]*2
+    def _printrng(evts):
+        if 'y' in evts:
+            vals[:2] = evts['y'].value
+    with bokehaction.launch('hybridstat.view.peaksplot.PeaksPlotView',
+                            'app.BeadToolBar') as server:
+        server.ctrl.observe("globals.project.plot.peaks", _printrng)
+        server.load('big_legacy')
+
+        krow = next(iter(server.doc.select(dict(type = DpxKeyedRow))))
+        def _press(val, *truth):
+            server.press(val, krow)
+            assert vals == approx(truth, rel = 1e-2)
+
+        fig = server.widget['Peaks:fig']()
+        for _ in range(5):
+            if fig.extra_x_ranges['duration'].end is None:
+                server.wait()
+        _press('Shift- ',         0.,       0.)
+        _press('Shift-ArrowUp',   0.216678, 0.377610)
+        _press('Alt-ArrowUp',     0.248864, 0.409797)
+        _press('Alt-ArrowDown',   0.216678, 0.377610)
+        _press('Shift-ArrowDown', -0.10518, 0.699475)
+
+        src = server.widget['Peaks:List'].source
+        assert all(np.isnan(src.data['distance']))
+        assert all(i.strip() == '' for i in src.data['orient'])
+        server.change('Cycles:Oligos', 'value', 'ctgt')
+        assert all(np.isnan(src.data['distance']))
+        assert all(i.strip() == '' for i in src.data['orient'])
+        server.change('Cycles:Oligos', 'value', '')
+        server.load('hairpins.fasta', andpress = False)
+        server.change('Cycles:Sequence', 'value', '←')
+        assert all(np.isnan(src.data['distance']))
+        assert all(i.strip() == '' for i in src.data['orient'])
+
+        server.change('Cycles:Oligos', 'value', 'ctgt')
+        assert server.widget['Cycles:Oligos'].value == 'ctgt'
+        assert not all(np.isnan(src.data['distance']))
+        assert not all(i.strip() == '' for i in src.data['orient'])
+
+        menu = server.widget['Cycles:Sequence'].menu
+        lst  = tuple(i if i is None else i[0] for i in list(menu))
+        assert lst == ('GF4', 'GF2', 'GF1', 'GF3', '015', None, 'Select sequence')
+
+def test_hybridstat(bokehaction):
+    "test hybridstat"
+    with bokehaction.launch('hybridstat.view', 'app.BeadToolBar') as server:
+        server.change('Hybridstat:Tabs', 'active', 0)
+        server.change('Hybridstat:Tabs', 'active', 1)
+        server.change('Hybridstat:Tabs', 'active', 2)
+
+        server.change('Hybridstat:Tabs', 'active', 1)
+        server.load('big_legacy')
+
+        server.change('Hybridstat:Tabs', 'active', 0)
+        server.change('Hybridstat:Tabs', 'active', 1)
+        server.change('Hybridstat:Tabs', 'active', 2)
+        server.change('Hybridstat:Tabs', 'active', 0)
+        server.change('Hybridstat:Tabs', 'active', 1)
+        server.change('Hybridstat:Tabs', 'active', 2)
+
+if __name__ == '__main__':
+    test_peaksplot(bokehaction(None))
