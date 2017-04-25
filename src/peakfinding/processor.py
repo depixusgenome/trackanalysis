19f11f87d466c4ef64f8bb7b43897f20d2fe80a6
Pol d Avezac <pol.davezac@depixus.com>
2017-04-25 21:37:09 +0200

    adding a phase 5 alignment task

--- src/peakfinding/processor.py
+++ src/peakfinding/processor.py
@@ -1,16 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-u"Tasks related to peakfinding"
+"Tasks related to peakfinding"
 from typing             import (Iterator, Tuple, # pylint: disable=unused-import
                                 Optional, Sequence, List, Set)
 
 from model.task         import Task, Level
 from control.processor  import Processor
 from data.trackitems    import BEADKEY, TrackItems, Beads
+from signalfilter       import rawprecision
+from .alignment         import PeakCorrelationAlignment
 from .selector          import PeakSelector, Output as PeakOutput
 
+class PeakCorrelationAlignmentTask(PeakCorrelationAlignment, Task):
+    "Aligns cycles using peaks"
+    level = Level.event
+    def __init__(self, **kwa):
+        Task.__init__(self)
+        super().__init__(**kwa)
+
+class PeakCorrelationAlignmentProcessor(Processor):
+    "Groups events per peak"
+    @classmethod
+    def __action(cls, frame, cnf):
+        cache = dict()
+        tsk   = PeakCorrelationAlignment(**cnf)
+        def _action(info):
+            nonlocal cache
+            deltas = cache.get(info[0][0], None)
+            if deltas is None:
+                precision = rawprecision(frame.data.track, info[0][0])
+                data      = tuple(i for _, i in frame[info[0][0], ...])
+                cache[info[0][0]] = deltas = tsk(data, precision)
+
+            info[1]['data'] += deltas[info[0][1]]
+            return info
+        return _action
+
+    @classmethod
+    def apply(cls, toframe = None, **cnf):
+        "applies the task to a frame or returns a function that does so"
+        # pylint: disable=not-callable
+        fcn = lambda frame: (frame
+                             .new()
+                             .withaction(cls.__action(frame, cnf), beadsonly = True))
+        return fcn if toframe is None else fcn(toframe)
+
+    def run(self, args):
+        args.apply(self.apply(**self.config()))
+
 class PeakSelectorTask(PeakSelector, Task):
-    u"Groups events per peak"
+    "Groups events per peak"
     levelin = Level.event
     levelou = Level.peak
     def __init__(self, **kwa):
@@ -19,7 +58,7 @@ class PeakSelectorTask(PeakSelector, Task):
 
 Output = Tuple[BEADKEY, Iterator[PeakOutput]]
 class PeaksDict(TrackItems):
-    u"iterator over peaks grouped by beads"
+    "iterator over peaks grouped by beads"
     level = Level.peak
     def __init__(self, *_, config = None, **kwa):
         assert len(_) == 0
@@ -45,7 +84,7 @@ class PeaksDict(TrackItems):
 
     def __run(self, ibead) -> Iterator[PeakOutput]:
         vals = iter(i for _, i in self.data[ibead,...])
-        prec = self.config.histogram.rawprecision(self.data.track, ibead)
+        prec = rawprecision(self.data.track, ibead)
         yield from self.config(vals, prec)
 
     def _iter(self, sel:Optional[Sequence] = None) -> Iterator[Output]:
@@ -54,11 +93,11 @@ class PeaksDict(TrackItems):
     def detailed(self, ibead, precision = None):
         "detailed output from config"
         if precision is None:
-            precision = self.config.histogram.rawprecision(self.data.track, ibead)
+            precision = rawprecision(self.data.track, ibead)
         return self.config.detailed(iter(i for _, i in self.data[ibead,...]), precision)
 
 class PeakSelectorProcessor(Processor):
-    u"Groups events per peak"
+    "Groups events per peak"
     @classmethod
     def apply(cls, toframe = None, **cnf):
         "applies the task to a frame or returns a function that does so"
