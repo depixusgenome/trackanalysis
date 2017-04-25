10684421e0f0b5536fe02df4fb3c6825d5a19dc2
Pol d Avezac <pol.davezac@depixus.com>
2017-04-25 22:11:56 +0200

    modal with hierarchical model & csv inputs
    - csv inputs supported: text inputs which can parse csv
    - hierarchical model: a key containing '.' provides access to attributes
      of attributes.

--- src/modaldialog/modal.py
+++ src/modaldialog/modal.py
@@ -7,10 +7,11 @@ from    typing                  import (Optional,  # pylint: disable=unused-impo
                                         Callable, Union, Sequence)
 from    abc                     import ABCMeta, abstractmethod
 import  re
-import  numpy as np
 
 import  bokeh.core.properties   as props
 from    bokeh.models            import Model, Callback
+from    utils.logconfig         import getLogger
+LOGS = getLogger()
 
 class Option(metaclass = ABCMeta):
     "Converts a text tag to an html input"
@@ -24,12 +25,57 @@ class Option(metaclass = ABCMeta):
         "returns a method which sets values in a model"
         raise NotImplementedError()
 
+    def _converter(self, model, elems, cnv, storeempty = None) -> Callable:
+        "returns a method which sets values in a model"
+        if storeempty is None:
+            def _empty(model, key):
+                if elems[key]:
+                    self._setattr(model, key, None)
+                elif self._cnv is str: # pylint: disable=no-member
+                    self._setattr(model, key, '')
+            storeempty = _empty
+
+        def _apply(key, val):
+            if key not in elems:
+                return False
+
+            if val != '' :
+                try:
+                    converted = cnv(val)
+                except Exception as exc: # pylint: disable=broad-except
+                    LOGS.exception(exc)
+                else:
+                    self._setattr(model, key, converted)
+            else:
+                storeempty(model, key)
+            return True
+        return _apply
+
+    @staticmethod
+    def _getattr(mdl, keystr, default):
+        keys = keystr.split('.')
+        for key in keys[:-1]:
+            mdl = getattr(mdl, key)
+        return getattr(mdl, keys[-1], default)
+
+    @staticmethod
+    def _setattr(mdl, keystr, val):
+        keys = keystr.split('.')
+        for key in keys[:-1]:
+            mdl = getattr(mdl, key)
+        setattr(mdl, keys[-1], val)
+
 class TextOption(Option):
     "Converts a text tag to an html text input"
     def __init__(self, cnv, patt, step):
         self._cnv  = cnv
         self._patt = re.compile(r'%\((.*?)\)'+patt)
         self._step = step
+
+    def converter(self, model, body:str) -> Callable:
+        "returns a method which sets values in a model"
+        elems = {i[0]: i[2] == 'o' for i in self._patt.findall(body)}
+        return self._converter(model, elems, self._cnv)
 
     def replace(self, model, body:str) -> str:
         "replaces a pattern by an html tag"
@@ -38,8 +84,10 @@ class TextOption(Option):
             opt  = (''          if size is None else
                     'step=1'    if size == 0    else
                     'step=0.'+'0'*(size-1)+'1')
-            if np.isscalar(getattr(model, key, None)):
-                opt += ' value={}'.format(getattr(model, key))
+
+            val  = self._getattr(model, key, None)
+            if val is not None:
+                opt += ' value="{}"'.format(val)
 
             inpt = '<input class="bk-widget-form-input" type="{}" name="{}" {}>'
             return inpt.format(tpe, key, opt)
@@ -51,30 +99,43 @@ class TextOption(Option):
             fcn = lambda i: _replace(i.group(1), self._step, tpe)
         return self._patt.sub(fcn, body)
 
+class CSVOption(Option):
+    "Converts a text tag to an html text input"
+    def __init__(self, cnv, patt):
+        super().__init__(self)
+        split      = re.compile('[,;:]').split
+        self._cnv  = lambda i: tuple(cnv(j) for j in split(i))
+        self._patt = re.compile(r'%\((.*?)\)'+patt)
+
     def converter(self, model, body:str) -> Callable:
         "returns a method which sets values in a model"
         elems = {i[0]: i[2] == 'o' for i in self._patt.findall(body)}
-        cnv   = self._cnv
-        def _apply(key, val):
-            opt = elems.get(key, None)
-            if opt is None:
-                return False
+        return self._converter(model, elems, self._cnv)
 
-            if val != '' :
-                setattr(model, key, cnv(val))
-            elif opt:
-                setattr(model, key, None)
-            elif cnv is str:
-                setattr(model, key, '')
-            return True
-        return _apply
+    def replace(self, model, body:str) -> str:
+        "replaces a pattern by an html tag"
+        def _replace(match):
+            key = match.group(1)
+            assert len(key), "keys must have a name"
+            val  = self._getattr(model, key, None)
+            opt  = (' value="{}"'.format(', '.join(str(i) for i in val))
+                    if val is not None else '')
+            opt += ' placeholder="comma separated values"'
+
+            inpt = '<input class="bk-widget-form-input" type="text" name="{}" {}>'
+            return inpt.format(key, opt)
+
+        return self._patt.sub(_replace, body)
 
 class DpxModal(Model):
     "Modal dialog"
     __INSTANCE         = None # type: Optional[DpxModal]
     __OPTIONS          = (TextOption(int,   r'(o)*[id]',         1),
                           TextOption(float, r'(?:\.(\d))*(o)*f', lambda i: i.group(2)),
-                          TextOption(str,   r'(o)*s',            None))
+                          TextOption(str,   r'(o)*s',            None),
+                          CSVOption(int,   r'(o)*csv[id]'),
+                          CSVOption(float, r'(?:\.(\d))*(o)*csvf'),
+                          CSVOption(str,   r'(o)*csv'))
     __css__            = ["consts/backbone.modal.css",
                           "consts/backbone.modal.theme.css"]
     __javascript__     = "consts/underscore-min.js"
