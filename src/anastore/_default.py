2280a652a9a41a1e5b78a1a91760082a72354b6b
Pol d Avezac <pol.davezac@depixus.com>
2017-05-18 22:14:48 +0200

    patching config file

--- src/anastore/_default.py
+++ src/anastore/_default.py
@@ -3,6 +3,15 @@
 u"""
 patches for tasks and configs
 """
-from ._patches  import Patches
-__TASKS__   = Patches()
-__CONFIGS__ = Patches()
+from ._patches  import Patches, modifyclasses
+
+def _v0(data:dict) -> dict:
+    modifyclasses(data,
+                  "eventdetection.processor.ExtremumAlignmentTask",
+                  dict(edge = lambda val: 'right' if val else None))
+
+    data.get('config', {}).pop('precision.max', None)
+    return data
+
+__TASKS__   = Patches(_v0)
+__CONFIGS__ = Patches(_v0)
--- src/anastore/_patches.py
+++ src/anastore/_patches.py
@@ -3,11 +3,13 @@
 u"""
 Patch mechanism
 """
-from typing import Callable, List # pylint: disable=unused-import
+from typing  import Callable, List # pylint: disable=unused-import
+from ._utils import TPE
+
 class Patches:
     u"This must contain json patches up to the app's versions number"
-    def __init__(self):
-        self._patches = [] # type: List[Callable]
+    def __init__(self, *patches):
+        self._patches = list(patches) # type: List[Callable]
 
     def patch(self, fcn: Callable):
         u"registers a patch"
@@ -30,7 +32,63 @@ class Patches:
         if vers < self.version:
             for fcn in self._patches[vers:]:
                 data = fcn(data)
+                assert data is not None
 
         elif vers > self.version:
             raise IOError("Anastore file version is too high", "warning")
         return data
+
+def modifyclasses(data, *args):
+    """
+    Scans the data applying listed patches.
+
+    The arguments should be a flat list of pairs:
+
+        >>> modifyclasses(data,
+        ...               "modulename1.classname1", dict(attr1 = lambda val: val*2),
+        ...               "modulename2.classname2", dict(attr2 = lambda val: val/2))
+
+    """
+    assert len(args) % 2 == 0
+    reps  = {args[2*i]: args[2*i+1] for i in range(len(args)//2)}
+    empty = {}
+    def _scan(itm):
+        if isinstance(itm, list):
+            for val in itm:
+                if isinstance(val, (dict, list)):
+                    _scan(val)
+
+        elif isinstance(itm, dict):
+            for val in itm.values():
+                if isinstance(val, (dict, list)):
+                    _scan(val)
+
+            cur = reps.get(itm.get(TPE, None), empty)
+            itm.update((key, cur[key](itm[key])) for key in set(itm) & set(cur))
+
+    _scan(data)
+
+def modifykeys(data, *args, **kwa):
+    """
+    Finds a specific key and modifies its value.
+
+        >>> data["key1"] = dict(key2 = 1)
+        >>> modifykey(data,  "key1", "key2", lambda val: val*2)
+        >>> assert data["key1"]["key2"] = 2
+
+        >>> data = {}
+        >>> modifykey(data,  "key1", "key2", lambda val: val*2)
+
+    """
+    for key in args[:None if len(kwa) else -2]:
+        if ((isinstance(data, dict) and key in data)
+                or isinstance(data, list) and len(data) > key):
+            data = data[key]
+        else:
+            return
+
+    if len(kwa):
+        data[args[-2]] =  args[-1](data[args[-2]])
+    else:
+        for key, val in kwa.items():
+            data[key] =  val(data[key])
