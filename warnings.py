0b3a952f1a868b3d40a32ad0f3fe8cee25cb5e63
Pol d Avezac <pol.davezac@depixus.com>
2017-07-06 16:03:39 +0200

    removing bkcharts warning

--- warnings.py
+++ warnings.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 "remove warnings"
+import sys
 import warnings
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category = FutureWarning)
@@ -8,11 +9,11 @@ with warnings.catch_warnings():
         import pandas.tslib # pylint: disable=unused-import
     except ImportError:
         pass
-    warnings.filterwarnings('ignore', category = DeprecationWarning)
-    try:
-        import bokeh.charts # discard bokeh warning # pylint: disable=unused-import
-    except ImportError:
-        pass
+try:
+    import bkcharts
+    sys.modules['bokeh.charts'] = bkcharts
+except ImportError:
+    pass
 warnings.filterwarnings('ignore',
                         category = DeprecationWarning,
                         message  = '.* deprecated in Bokeh 0.12.6 .*')
