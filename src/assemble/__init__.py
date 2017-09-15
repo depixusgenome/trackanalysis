617912c4a26c929f2e75ceb22b3be11b7daa9c7b
David Salthouse <david.salthouse@depixus.com>
2017-09-15 10:38:43 +0200

    removing assemble from track

--- src/assemble/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-u"All code related to sequencing hairpins"
-
-from . import shuffler as shuffler
-from . import data as asmdata
-from .assembler import MCAssembler, NestedAsmrs
-from .recorder import Recorder, SeqRecorder
