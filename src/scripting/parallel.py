63894fe00c5d862de8cd4fcea1eab2edd5f1d978
Pol d Avezac <pol.davezac@depixus.com>
2017-09-23 21:19:57 +0200

    adding helper class for parallelizing tasks

---
+++ src/scripting/parallel.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"Runs tasks in parallel"
+from typing                 import (Union, Sequence, Type, Dict, Generator,
+                                    Callable, Iterator, cast)
+from concurrent.futures     import ProcessPoolExecutor, ThreadPoolExecutor
+from multiprocessing        import cpu_count
+import pickle
+import pandas               as     pd
+
+from model.task.track       import TrackReaderTask, RootTask, Task
+from control.taskcontrol    import register, create
+from control.processor      import Processor
+from data.views             import TrackView
+from data.tracksdict        import TracksDict
+
+class Parallel:
+    "Runs tasks in parallel"
+    def __init__(self,
+                 roots     : Union[TracksDict, Sequence[RootTask]],
+                 *tasks    : Task,
+                 processors: Dict[Type[Task], Type[Processor]] = None) -> None:
+        if not isinstance(roots, TracksDict):
+            lroots = list(cast(Iterator[RootTask], roots))
+        else:
+            lroots = [TrackReaderTask(path = i.path, key  = i.key, axis = i.axis.value)
+                      for i in cast(TracksDict, roots).values()]
+
+        procs     = (register(processors) if not isinstance(processors, dict) else
+                     processors)
+        toproc    = lambda i: cast(Processor,
+                                   (i if isinstance(i, Processor) else
+                                    procs[type(i)](i))
+                                  )
+        main      = [toproc(i) for i in tasks]
+        self.args = [pickle.dumps([toproc(i)]+main) for i in lroots]
+
+    def process(self,
+                pool: Union[ProcessPoolExecutor, ThreadPoolExecutor] = None,
+                endaction: Union[str, Callable] = None):
+        "processes the parallel task"
+        if pool is None:
+            pool = ProcessPoolExecutor(cpu_count())
+
+        if endaction in (pd.concat, 'concat'):
+            return pd.concat(sum((list(i) for i in pool.map(self.run, self.args)), []))
+
+        elif callable(endaction):
+            return [cast(Callable, endaction)(i) for i in pool.map(self.run, self.args)]
+
+        else:
+            res = [i for i in pool.map(self.run, self.args)]
+            if all(len(i) == 1 for i in res):
+                return [i[0] for i in res]
+            return res
+
+    @staticmethod
+    def run(args: bytes):
+        "runs one task"
+        def _cnv(res):
+            if isinstance(res, TrackView):
+                res = tuple(res)
+                if len(res) and len(res[0]) == 2 and isinstance(res[0][1], Generator):
+                    res = tuple((i, tuple(j)) for i, j in res)
+            return res
+
+        res = tuple(_cnv(i) for i in create(pickle.loads(args)).run())
+        return res
+
+def parallel(roots     : Union[TracksDict, Sequence[RootTask]],
+             *tasks    : Task,
+             processors: Dict[Type[Task], Type[Processor]]              = None,
+             pool      : Union[ProcessPoolExecutor, ThreadPoolExecutor] = None,
+             endaction : Callable                                       = None):
+    "Runs tasks in parallel"
+    return Parallel(roots, *tasks, processors = processors).process(pool, endaction)
