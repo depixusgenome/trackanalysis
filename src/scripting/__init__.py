5ee303ec21c074b667ce2d52b08050e94ad03dbc
Pol d Avezac <pol.davezac@depixus.com>
2017-11-14 10:51:17 +0100

    Adding a LazyShelf object for storing intermediate steps in the analysis

--- src/scripting/__init__.py
+++ src/scripting/__init__.py
@@ -53,6 +53,7 @@ from cleaning.__scripting__       import *
 from eventdetection.__scripting__ import *
 from peakfinding.__scripting__    import *
 from peakcalling.__scripting__    import * # type: ignore
+from .datadump                    import LazyShelf
 
 LOGS = getLogger(__name__)
 
