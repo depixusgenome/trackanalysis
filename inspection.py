ceb3bd15e32370900d715efa17340c4d932b2f22
Pol d'Avezac <pol.davezac@depixus.com>
2017-12-04 08:31:08 +0100

    adding access to template attributes

--- inspection.py
+++ inspection.py
@@ -11,6 +11,15 @@ signature           = inspect.signature
 getmembers          = inspect.getmembers
 isgeneratorfunction = inspect.isgeneratorfunction
 
+def templateattribute(cls, index) -> type:
+    "returns a template attribute"
+    cur  = cls
+    orig = getattr(cls, '__orig_bases__')
+    while orig is None or orig[0].__args__ is None:
+        cur  = getattr(cur, '__base__')
+        orig = getattr(cur, '__orig_bases__', None)
+    return orig[0].__args__[index]    # type: ignore
+
 def isfunction(fcn) -> bool:
     u"Returns whether the object is a function"
     return isinstance(fcn, (LambdaType, FunctionType, MethodType, partial))
