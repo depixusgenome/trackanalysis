9c7a092fd3d1f372c801fce9d2a041f94ee10d3a
Pol d Avezac <pol.davezac@depixus.com>
2018-04-18 15:45:04 +0200

    adapting admin lua

--- src/daq/server/adminview.py
+++ src/daq/server/adminview.py
@@ -1,48 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 "Sending orders to the server"
-import socket
-import struct
-import asyncio
-
-from   contextlib           import closing
 from   enum                 import Enum
-from   typing               import Tuple, Coroutine
+from   typing               import Tuple, Coroutine, cast
 
 import websockets
-import numpy                as     np
 
 from   tornado.ioloop       import IOLoop
 
 from   ..model              import DAQBead, DAQProtocol, DAQNetwork, DAQManual
-
-async def send2daq(cnf, text):
-    "sends a Lua script to the DAQ"
-    async with websockets.connect(cnf.websocket) as websocket:
-        await websocket.send(text)
-
-async def readdaq(cnf, output):
-    """
-    Reads server data and outputs it
-    """
-    pack     = struct.pack('4sL', socket.inet_aton(cnf.multicast), socket.INADDR_ANY)
-    data     = np.zeros(cnf.packet, cnf.columns)
-    address  = cnf.address
-    period   = 1./cnf.rate
-    bytesize = cnf.bytesize
-    offset   = cnf.offset
-    rng      = range(len(data)-1)
-    dontstop = True
-    while dontstop:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(address)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, pack)
-        with closing(sock):
-            data[0] = sock.recvfrom(bytesize)[0][offset:]
-            for i in rng:
-                await asyncio.sleep(period)
-                data[i] = sock.recvfrom(bytesize)[0][offset:]
-            dontstop = output(data)
 
 class Teensy(Enum):
     """
@@ -63,12 +29,17 @@ class DAQAdmin: # pylint: disable=too-many-public-methods
     Allows sending orders to the DAQ server
     """
     _CMD    = "local {} = hidCommand.new();"
-    _SCRIPT = "local {} = hidCommandScript.new();"
-    _ADD    = "{}.insert({});"
-    _SEND   = "daq_sendCommand({});"
+    _SCRIPT = "local {} = hidCommandScript.new({}, 0);"
+    _INSERT = "{}.insert({});"
+    _SEND   = "daq_sendTo({});"
     _PREC   = dict(zmag = 1e-3,   x = 1e-3, y = 1e-3, tsample = 1e-2)
     def __init__(self, model: DAQNetwork) -> None:
-        self.model    = model
+        self.model = model
+
+    async def send(self, text):
+        "sends a Lua script to the DAQ"
+        async with websockets.connect(self.model.websocket) as websocket:
+            await websocket.send(text)
 
     def run(self, fcn, *args):
         "runs a coroutine"
@@ -77,55 +48,27 @@ class DAQAdmin: # pylint: disable=too-many-public-methods
                 fcn = getattr(self, fcn)
             IOLoop.current().spawn_callback(fcn, *args)
 
-    async def getconfig(self) -> dict:
-        "get the beads tracked by the server"
-        raise NotImplementedError()
-
-    async def getcamera(self) -> dict:
-        "get the beads tracked by the server"
-        cnf = await self.getconfig()
-        cam = cnf['daqserver']['devices']['camera']
-        return {'pixels'    : (cam['aoiwidth'], cam['aoiheight']),
-                'framerate' : cam['framerate']
-               }
-
-    async def getbeads(self) -> Tuple[DAQBead, ...]:
-        "get the beads tracked by the server"
-        raise NotImplementedError()
-
-    async def setbeads(self, beads: Tuple[DAQBead, ...]):
-        "set the beads tracked by the server"
-        raise NotImplementedError()
-
-    def setprotocol(self, protocol: DAQProtocol) -> Coroutine:
+    def setprotocol(self, protocol: DAQProtocol, test = False) -> Coroutine:
         "set info about the current protocol by the server"
-        return send2daq(self.model, self._setprotocol(protocol))
-
-    def getprotocol(self) -> DAQProtocol:
-        "set info about the current protocol by the server"
-        raise NotImplementedError()
-
-    async def wait(self, *args):
-        "wait for a value to be reached"
-        if len(args) == 1:
-            await asyncio.sleep(args[0])
+        setv  = lambda n, x, y: cast(str, self.setvalues(Teensy.zmag,      x.zmag,
+                                                         Teensy.vmag,      x.vmag,
+                                                         Teensy.startTime, y,
+                                                         name = n, test = True))
+        if isinstance(protocol, DAQManual):
+            msg = setv("s", protocol, None)
         else:
-            def _done(lines):
-                return all(abs(lines[-1][args[i]]-args[i+1]) < self._PREC[args[i]]
-                           for i in range(0, len(args), 2))
-            await readdaq(self.model, _done)
+            lst   = protocol.phases
+            phase = [i for i in lst if i.zmag is not None][-1]
+            msg   = (setv("f", phase, None)
+                     + self._SEND.format("f")
+                     + ''.join(setv(f"p{i}", cur, lst[i-1].duration)
+                               for i, cur in enumerate(lst))
+                     + self._SCRIPT.format("s", protocol.cyclecount)
+                     + ''.join(self._INSERT.format("s", f"p{i}")
+                               for i in range(len(lst))))
 
-    async def setrecording(self, start: bool, record: str):
-        """
-        tell the server to start recording
-        """
-        raise NotImplementedError()
-
-    async def getrecording(self):
-        """
-        tell the server to start recording
-        """
-        raise NotImplementedError()
+        msg += self._SEND.format("s")
+        return msg if test else self.send(msg)
 
     def record(self, record: str):
         """
@@ -145,35 +88,50 @@ class DAQAdmin: # pylint: disable=too-many-public-methods
         async def __aexit__(self, *_):
             await self.admin.setrecording(False)
 
-    def setvalues(self, *args) -> Coroutine:
-        "sets a list of values"
-        msg = self._setvalues("t", *args)+self._SEND.format("t")
-        return send2daq(self.model, msg)
-
-    def _setprotocol(self, protocol: DAQProtocol) -> str:
-        setv  = lambda n, x, y: self._setvalues(n,
-                                                Teensy.zmag,      x.zmag,
-                                                Teensy.vmag,      x.vmag,
-                                                Teensy.startTime, y)
-        if isinstance(protocol, DAQManual):
-            return setv("s", protocol, None) + self._SEND.format("s")
-
-        lst   = protocol.phases
-        phase = [i for i in lst if i.zmag is not None][-1]
-        return (setv("f", phase, None)
-                + self._SEND.format("f")
-                + ''.join(setv(f"p{i}", cur, lst[i-1].duration) for i, cur in enumerate(lst))
-                + self._SCRIPT.format("s")
-                + ''.join(self._ADD.format("s", f"p{i}") for i in range(len(lst)))
-                + self._SEND.format("s"))
-
-    def _setvalues(self, name, *args) -> str:
+    def setvalues(self, *args, name = "t", test = False) -> Coroutine:
         "sets a list of values"
         msg  = self._CMD.format(name)
         for i in range(0, len(args), 2):
             if args[i+1] is not None:
                 msg += "{}.{}={};".format(name, Teensy(args[i]).value, args[i+1])
-        return msg
+        msg += self._SEND.format("t")
+        return msg if test else self.send(msg)
+
+    async def setbeads(self, beads: Tuple[DAQBead, ...]):
+        "set the beads tracked by the server"
+        raise NotImplementedError()
+
+    async def setrecording(self, start: bool, record: str):
+        """
+        tell the server to start recording
+        """
+        raise NotImplementedError()
+
+    async def getconfig(self) -> dict:
+        "get the beads tracked by the server"
+        raise NotImplementedError()
+
+    async def getcamera(self) -> dict:
+        "get the beads tracked by the server"
+        cnf = await self.getconfig()
+        cam = cnf['daqserver']['devices']['camera']
+        return {'pixels'    : (cam['aoiwidth'], cam['aoiheight']),
+                'framerate' : cam['framerate']
+               }
+
+    async def getbeads(self) -> Tuple[DAQBead, ...]:
+        "get the beads tracked by the server"
+        raise NotImplementedError()
+
+    async def getprotocol(self) -> DAQProtocol:
+        "set info about the current protocol by the server"
+        raise NotImplementedError()
+
+    async def getrecording(self):
+        """
+        tell the server to start recording
+        """
+        raise NotImplementedError()
 
 class DAQAdminView:
     """
