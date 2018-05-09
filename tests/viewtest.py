39db9ebf15de3756298325eedc1b27c49033dd87
Pol d'Avezac <pol.davezac@depixus.com>
2018-05-09 22:01:11 +0200

    removing unused view

--- tests/viewtest.py
+++ tests/viewtest.py
@@ -102,36 +102,5 @@ def test_beadtoolbar(bokehaction):
         server.change('Main:toolbar', 'discarded', '0')
         assert frozenset(beads.availablebeads) == frozenset((1,))
 
-def test_beadplot(bokehaction):
-    "test plot"
-    vals = [0.]*4
-    def _printrng(evts):
-        if 'x' in evts:
-            vals[:2] = [0. if i is None else i for i in evts['x'].value]
-        if 'y' in evts:
-            vals[2:] = [0. if i is None else i for i in evts['y'].value]
-
-    with bokehaction.launch('view.beadplot.BeadPlotView', 'app.toolbar') as server:
-        server.ctrl.observe("globals.project.plot.bead", _printrng)
-        server.ctrl.observe("rendered", lambda *_1, **_2: server.wait())
-        server.load('small_legacy', andstop = False)
-
-        krow = next(iter(server.doc.select(dict(type = DpxKeyedRow))))
-        def _press(val, *truth):
-            server.press(val, krow)
-            assert vals == approx(truth, rel = 1e-2)
-
-        _press('Shift- ',          0.,       0.,         0.,      0.)
-        _press('Shift-ArrowUp',    0.,       0.,         0.41992, 0.65178)
-        _press('Shift-ArrowRight', 851.7503, 951.2497,   0.41992, 0.65178)
-        _press('Alt-ArrowLeft',    831.8504, 931.34982,  0.41992, 0.65178)
-        _press('Alt-ArrowUp',      831.8504, 931.34982,  0.46629, 0.69815)
-        _press('Alt-ArrowRight',   851.7503, 951.2497,   0.46629, 0.69815)
-        _press('Alt-ArrowDown',    851.7503, 951.2497,   0.41992, 0.65178)
-        _press('Shift-ArrowLeft',  0.,       0.,         0.41992, 0.65178)
-        _press('Shift-ArrowDown',  0.,       0.,         0.,      0.)
-        _press('Shift-ArrowUp',    0.,       0.,         0.41992, 0.65178)
-        server.press('Ctrl-z')
-
 if __name__ == '__main__':
     test_toolbar(bokehaction(None))
