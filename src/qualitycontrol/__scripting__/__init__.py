d83fd9c75babd9546893dec2a01178c174177743
Pol d'Avezac <pol.davezac@depixus.com>
2018-08-03 13:17:25 +0200

    adding as many lines as there are measures in a cycle

--- src/qualitycontrol/__scripting__/__init__.py
+++ src/qualitycontrol/__scripting__/__init__.py
@@ -17,23 +17,30 @@ def qcdataframe(items: Union[Track, TracksDict])-> pd.DataFrame:
     return the temperatures in a  dataframe
     """
     def _compute(track):
-        length = np.nanmean(np.diff(track.phases[:,0]))
+        sizes  = np.diff(np.append(track.phases[:,0], track.nframes+track.phases[0,0]))
+        length = np.concatenate([np.full(j, i, dtype = 'i4') for i, j in enumerate(sizes)]
+                                +[np.full(1000, -1, dtype = 'i4')])
+
         get    = lambda i, j: getattr(track.secondaries, i)[j]
         data   = lambda i: get(i, 'value')
-        index  = lambda i: np.int32(np.round(get(i, 'index')/length))
+        index  = lambda i: (get(i, 'index')-track.phases[0,0]).astype('i4')
+        cycle  = lambda i: length[index(i)]
 
         dframe: pd.DataFrame = None
         for i in ("tservo", "tsink", "tsample"):
-            tmp    = pd.DataFrame({i: data(i)}, index = index(i))
-            dframe = tmp if dframe is None else dframe.join(tmp) # type: ignore
+            tmp    = pd.DataFrame({i: data(i), 'index': index(i), 'cycle': cycle(i)})
+            tmp.set_index(['cycle', 'index'], inplace = True)
+            dframe = tmp if dframe is None else dframe.join(tmp, how = 'outer') # type: ignore
 
         vca    = track.secondaries.vcap
         if vca is None:
             dframe = dframe.assign(zmag = np.full(len(dframe), np.NaN, dtype = 'f4'),
                                    vcap = np.full(len(dframe), np.NaN, dtype = 'f4'))
         else:
-            dframe = dframe.join(pd.DataFrame({'zmag' : vca['zmag'], 'vcap' : vca['vcap']},
-                                              index = index("vcap")))
+            tmp = pd.DataFrame({'zmag' : vca['zmag'], 'vcap' : vca['vcap'],
+                                'index': index("vcap"), 'cycle': cycle("vcap")})
+            tmp.set_index(['cycle', 'index'], inplace = True)
+            dframe = dframe.join(tmp, how = 'outer')
         return dframe.assign(track = np.full(len(dframe), trackname(track)))
 
     return pd.concat([_compute(i) for i in getattr(items, 'values', lambda : (items,))()])
