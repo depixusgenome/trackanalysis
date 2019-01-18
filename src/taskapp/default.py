8401f14e16ea9faaca22f0cc463a70e2474a0960
Pol d'Avezac <pol.davezac@depixus.com>
2019-01-18 10:59:00 +0100

    :bug: scripting did not work anymore

--- src/taskapp/default.py
+++ src/taskapp/default.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 "Updates app manager so as to deal with controllers"
 from app.launcher  import setup
+from .maincontrol  import createview
 
 VIEWS       = ('undo.UndoView', 'taskview.tasksview.TasksView',)
 CONTROLS    = ('taskcontrol.taskcontrol.TaskController',
                'taskstore.control',
                'undo.UndoController')
 
-setup(locals(), defaultcontrols = CONTROLS, defaultviews = VIEWS)
+setup(locals(), creator = createview, defaultcontrols = CONTROLS, defaultviews = VIEWS)
