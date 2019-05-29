00cc172c5eb28fac5add2d4c3698f3c00ac85e6e
Pol d'Avezac <pol.davezac@depixus.com>
2019-05-29 10:01:17 +0200

    :bug: could not run tests

--- tests/testingcore/__init__.py
+++ tests/testingcore/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ access to files """
+from pathlib     import Path
+from typing      import cast
 from ..testutils import (
-        ResourcePath, getmonkey, DummyPool, Path, cast
+        ResourcePath, getmonkey, DummyPool
 )
 
 def _trackreadertask(fpath):
